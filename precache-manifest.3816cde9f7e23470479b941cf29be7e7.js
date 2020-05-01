104cce8e77cefbc271fde89e7a2ced6dac657bcb
elganiesta <54721994+elganiesta@users.noreply.github.com>
2020-05-01 18:04:41 +0000

    Updates

---
+++ precache-manifest.3816cde9f7e23470479b941cf29be7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5522f494d4f25b79b4387905421a4162",
+    "url": "/my-Robots-friends/index.html"
+  },
+  {
+    "revision": "d5c31f2cdf22fdb2f645",
+    "url": "/my-Robots-friends/static/css/main.b62a391e.chunk.css"
+  },
+  {
+    "revision": "f967340a5df17f6f457d",
+    "url": "/my-Robots-friends/static/js/2.263af2c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-Robots-friends/static/js/2.263af2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c31f2cdf22fdb2f645",
+    "url": "/my-Robots-friends/static/js/main.9b26d100.chunk.js"
+  },
+  {
+    "revision": "0e1ed46c5952ec6c985b",
+    "url": "/my-Robots-friends/static/js/runtime-main.766df1ab.js"
+  }
+]);
